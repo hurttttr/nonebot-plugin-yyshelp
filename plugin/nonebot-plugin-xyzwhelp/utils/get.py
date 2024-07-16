fa7f2a77b02f9993a752f0900e3078a14acc4d38
hurttttr <945399100@qq.com>
2024-07-16 21:31:30 +0800

    修改宝箱计算正则提取

--- plugin/nonebot-plugin-xyzwhelp/utils/get.py
+++ plugin/nonebot-plugin-xyzwhelp/utils/get.py
@@ -1,9 +1,10 @@
-from nonebot import get_driver
-from io import BytesIO
-from PIL import Image
-import requests
 import json
 import re
+from io import BytesIO
+
+import requests
+from nonebot import get_driver
+from PIL import Image
 
 from ..config import Config
 from .calculate import calculate
@@ -14,10 +15,10 @@ config: Config = Config.parse_obj(get_driver().config)
 def get_image_and_cut(url: str) -> None:
     """
     从给定的URL获取图像并进行裁剪。
-    
+
     参数:
     url: 图像的URL地址。
-    
+
     返回值:
     无
     """
@@ -29,20 +30,20 @@ def get_image_and_cut(url: str) -> None:
     width, height = image.size
 
     # 裁剪图像的上部区域并保存
-    image.crop((0, height * 0.23, width, height * 0.3)).save(r'temp/cut1.jpg')
+    image.crop((0, height * 0.23, width, height * 0.3)).save(r"temp/cut1.jpg")
     # 裁剪图像的下部区域，转换为灰度并保存
-    image.crop((0, height * 0.75, width,
-                height * 0.87)).convert('L').save(r'temp/cut2.jpg')
+    image.crop((0, height * 0.75, width, height * 0.87)).convert("L").save(
+        r"temp/cut2.jpg"
+    )
 
 
-def ocr_space_file(filename,
-                   overlay=False,
-                   api_key=config.xyzwhelp_apikey,
-                   language='eng') -> str:
+def ocr_space_file(
+    filename, overlay=False, api_key=config.xyzwhelp_apikey, language="eng"
+) -> str:
     """
     使用本地文件通过OCR.space API进行请求。
     Python3.5 - 未在2.7上进行测试
-    
+
     :param filename: 文件路径及名称。
     :param overlay: 是否在响应中需要OCR.space的叠加层。默认为False。
     :param api_key: OCR.space的API密钥。默认为'helloworld'。
@@ -54,15 +55,15 @@ def ocr_space_file(filename,
 
     # 准备请求参数
     payload = {
-        'isOverlayRequired': overlay,
-        'apikey': api_key,
-        'language': language,
-        'OCREngine': 2,
+        "isOverlayRequired": overlay,
+        "apikey": api_key,
+        "language": language,
+        "OCREngine": 2,
     }
     # 打开文件并使用requests库发送POST请求
-    with open(filename, 'rb') as f:
+    with open(filename, "rb") as f:
         r = requests.post(
-            'https://api.ocr.space/parse/image',
+            "https://api.ocr.space/parse/image",
             files={filename: f},
             data=payload,
         )
@@ -73,19 +74,17 @@ def ocr_space_file(filename,
 def process_data(data: str) -> int:
     """
     处理数据字符串，将其中的字符'o', 'O' 替换成 '0'，将 'l', 'L', 'I', 'i' 替换成 '1'，然后将处理后的字符串转换成整数返回。
-    
+
     参数:
     data: str - 需要处理的数据字符串。
-    
+
     返回值:
     int - 经过字符替换并转换为整数后的结果。
     """
     # 替换所有的'o'和'O'为'0'
-    data = data.replace('o', '0').replace('O', '0')
+    data = data.replace("o", "0").replace("O", "0")
     # 替换所有的'l', 'L', 'I', 'i'为'1'
-    data = data.replace('l', '1').replace('L',
-                                          '1').replace('I',
-                                                       '1').replace('i', '1')
+    data = data.replace("l", "1").replace("L", "1").replace("I", "1").replace("i", "1")
     # 将处理后的字符串转换为整数并返回
     return int(data)
 
@@ -93,26 +92,27 @@ def process_data(data: str) -> int:
 def get_return_str() -> str:
     """
     从两张图片中提取信息，并根据提取的信息计算返回一个字符串结果。
-    
+
     函数流程：
     1. 从'cut1.jpg'图片中使用OCR技术提取文本，并提取预编码（格式如：数字/数字）。
     2. 从'cut2.jpg'图片中使用OCR技术提取文本，提取四种材料的信息（wooden, silver, gold, platinum）。
     3. 对提取的材料信息进行处理。
     4. 使用预编码和处理后的材料信息进行计算，返回计算结果。
-    
+
     返回:
     str: 计算后的字符串结果。
     """
     # 从'cut1.jpg'图片中提取预编码
-    result1 = json.loads(ocr_space_file(
-        r'temp/cut1.jpg', language='chs'))['ParsedResults'][0]['ParsedText']
-    pre_code = re.findall(r'\d+/\d+', result1)[0].split('/')[0]
+    result1 = json.loads(ocr_space_file(r"temp/cut1.jpg", language="chs"))[
+        "ParsedResults"
+    ][0]["ParsedText"]
+    pre_code = re.findall(r"\d+", result1)[0].split("/")[0]
 
     # 从'cut2.jpg'图片中提取四种材料的信息
     result2 = re.findall(
-        r'X[\da-zA-Z]+',
-        json.loads(ocr_space_file(r'temp/cut2.jpg'))['ParsedResults'][0]
-        ['ParsedText'])
+        r"X[\da-zA-Z]+",
+        json.loads(ocr_space_file(r"temp/cut2.jpg"))["ParsedResults"][0]["ParsedText"],
+    )
 
     # 对提取的材料信息进行处理
     wooden = process_data(result2[0][1:])
